fe251762bb54f2a31db38b8428f07cfe06cd839d
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-07-05 14:46:07 +0300

    More tests

--- test/index.js
+++ test/index.js
@@ -4,21 +4,79 @@ describe('classname-manipulator', function () {
   var assert = require('assert');
   var cm = require('..');
 
-  it('button', function () {
-    var props = {className: 'button'};
-    var styles = {button: 'button'};
-    assert(cm(props, styles), 'button');
+  describe('Testing the "props" argument transformation', function () {
+    it('button', function () {
+      var props = {
+        className: 'button'
+      };
+
+      assert.equal(cm(props), 'button');
+    });
+
+    it('button button--action', function () {
+      var props = {
+        className: 'button',
+        theme: {action: true}
+      };
+
+      assert.equal(cm(props), 'button button--action');
+    });
+
+    it('button button--size-m', function () {
+      var props = {
+        className: 'button',
+        theme: {size: 'm'}
+      };
+
+      assert.equal(cm(props), 'button button--size-m');
+    });
+
+    it('button button--action button--size-m', function () {
+      var props = {
+        className: 'button',
+        theme: {action: true, size: 'm'}
+      };
+
+      assert.equal(cm(props), 'button button--action button--size-m');
+    });
   });
 
-  it('button--action', function () {
-    var props = {className: 'button', theme: {action: true}};
-    var styles = {'button--action': 'button--action'};
-    assert(cm(props, styles), 'button--action');
-  });
+  describe('Testing the "styles" argument transformation', function () {
+    it('___button', function () {
+      var props = {className: 'button'};
+      var styles = {button: '___button'};
 
-  it('button--theme-action', function () {
-    var props = {className: 'button', theme: {theme: 'action'}};
-    var styles = {'button--theme-action': 'button--theme-action'};
-    assert(cm(props, styles), 'button--theme-action');
+      assert.equal(cm(props, styles), '___button');
+    });
+
+    it('___button with theme', function () {
+      var props = {
+        className: 'button',
+        theme: {action: true}
+      };
+      var styles = {button: '___button'};
+
+      assert.equal(cm(props, styles), '___button');
+    });
+
+    it('___button ___button--action', function () {
+      var props = {
+        className: 'button',
+        theme: {action: true}
+      };
+      var styles = {button: '___button', 'button--action': '___button--action'};
+
+      assert.equal(cm(props, styles), '___button ___button--action');
+    });
+
+    it('___button--action', function () {
+      var props = {
+        className: 'button',
+        theme: {action: true}
+      };
+      var styles = {'button--action': '___button--action'};
+
+      assert.equal(cm(props, styles), '___button--action');
+    });
   });
 });
